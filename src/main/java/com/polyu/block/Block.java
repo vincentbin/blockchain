f493f65e6eafa6d4420ec4278455cf9984270822
vincent <527500791@qq.com>
2022-03-07 16:17:04 +0800

    'new'

--- src/main/java/com/polyu/block/Block.java
+++ src/main/java/com/polyu/block/Block.java
@@ -1,7 +1,50 @@
 package com.polyu.block;
 
-public class Block {
-    public static void main(String[] args) {
+import com.polyu.util.HashUtil;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class Block {
+    private static final Logger logger = LoggerFactory.getLogger(Block.class);
+
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private long nonce;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calHash();
+    }
+
+    public String calHash() {
+        return HashUtil.encode(this.previousHash + this.timeStamp + this.nonce + this.data);
+    }
+
+    public static void main(String[] args) {
+        new Block("aaa", "0").mineBlock(7);
+    }
+
+    /**
+     * mine
+     * @param difficulty 不要超过 18 Math.pow 所限
+     */
+    public void mineBlock(int difficulty) {
+        long num = (long) Math.pow(10, difficulty);
+        String targetPrefix = Long.toString(num).substring(1);
+        while (!this.hash.substring(0, difficulty).equals(targetPrefix)) {
+            nonce++;
+            hash = calHash();
+        }
+        logger.info("mining success! hash: {}.", this.hash);
     }
 }
--- src/main/java/com/polyu/block/Chain.java
+++ src/main/java/com/polyu/block/Chain.java
@@ -1,4 +1,64 @@
 package com.polyu.block;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Chain {
+    private static final Logger logger = LoggerFactory.getLogger(Chain.class);
+
+    private static List<Block> blocks = new LinkedList<>();
+
+    public static void main(String[] args) {
+        blocks.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        //blocks.get(0).mineBlock(difficulty);
+
+        blocks.add(new Block("Yo im the second block",blocks.get(blocks.size()-1).getHash()));
+        System.out.println("Trying to Mine block 2... ");
+        //blocks.get(1).mineBlock(difficulty);
+
+        blocks.add(new Block("Hey im the third block", blocks.get(blocks.size()-1).getHash()));
+        System.out.println("Trying to Mine block 3... ");
+        //blocks.get(2).mineBlock(difficulty);
+
+        System.out.println("Blockchain is Valid: " + isValid());
+
+        System.out.println("blocks = " + blocks);
+
+    }
+
+    /**
+     * 验证区块链有效性
+     * @return
+     */
+    public static boolean isValid() {
+        if (blocks.isEmpty()) {
+            return true;
+        }
+        Iterator<Block> iterator = blocks.iterator();
+        Block pre = iterator.next();
+        if (!pre.getHash().equals(pre.calHash())) {
+            logger.warn("current block is changed.");
+            return false;
+        }
+        while (iterator.hasNext()) {
+            Block cur = iterator.next();
+            if (!cur.getHash().equals(cur.calHash())) {
+                logger.warn("current block is changed.");
+                return false;
+            }
+
+            if (!pre.getHash().equals(cur.getPreviousHash())) {
+                logger.warn("cur's pre Hash value is false.");
+                return false;
+            }
+            pre = cur;
+        }
+        return true;
+    }
+
 }
