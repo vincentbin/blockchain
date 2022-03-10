e170eb27c9294c0d9743c086f2310549e32b42d6
vincent <527500791@qq.com>
2022-03-10 13:29:19 +0800

    fix

--- src/main/java/com/polyu/blockchain/p2p/netty/server/Server.java
+++ src/main/java/com/polyu/blockchain/p2p/netty/server/Server.java
@@ -3,7 +3,6 @@ package com.polyu.blockchain.p2p.netty.server;
 
 import com.polyu.blockchain.common.p2p.P2PHolder;
 import com.polyu.blockchain.p2p.netty.BusinessHandler;
-import lombok.Data;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -21,8 +20,6 @@ public class Server implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-//        String serverAddress = "127.0.0.1:8001";
-//        String registryAddress = "127.0.0.1:2181";
         Thread thread = new Thread(new BootStrap(serverAddress, registryAddress));
         P2PHolder.setHandler(BusinessHandler.getInstance());
         thread.start();
